4c07f88eada7ea65429fd4dfc046111e606c1bf0
Stefano <14887091+stebo71@users.noreply.github.com>
2024-10-29 13:59:40 +0100

    terminata 11, aggiunto controller

--- app/Http/Controllers/ProvaController.php
+++ app/Http/Controllers/ProvaController.php
@@ -6,11 +6,13 @@ use Illuminate\Http\Request;
 
 class ProvaController extends Controller
 {
-    public function provaFunction() {
-        $calcolo = 5+5;
+    public function provaFunction()
+    {
+        $calcolo = 5+7;
         return $calcolo;
     }
-    public function provaData(Request $request) {
-        return 'Dati ricevuti:' . $request->input('data');
+    public function provaData(Request $request)
+    {
+        return 'Dati ricevuti: ' . $request->input('data');
     }
 }
